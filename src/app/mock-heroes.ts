94484f84f18c4f27eb6956538e26e65ed881f7cb
renemorenow <renemorenow@gmail.com>
2018-07-29 12:43:41 -0500

    1er publicacion en GIT de la aplicación

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,24 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Relámpago' },
+  { id: 2, name: 'Tormenta' },
+  { id: 3, name: 'Palomo' },
+  { id: 4, name: 'Corcel Negro' },
+  { id: 5, name: 'Flash' },
+  { id: 6, name: 'Macarena' },
+  { id: 7, name: 'Guajira' },
+  { id: 8, name: 'Cien Fuegos' },
+  { id: 9, name: 'El Furioso' },
+  { id: 10, name: 'Hannibal' },
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
